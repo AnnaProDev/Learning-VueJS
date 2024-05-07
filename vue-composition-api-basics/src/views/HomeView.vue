b51f7f328f02dfd2e287578f8aaa0786dd11f8cf
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2024-05-07 13:41:46 -0400

    Added test exercise for forms: adabtable button

---
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="home">
+		<div class="counter_wrapper">
+			<div class="button">+</div>
+			<span class="counter">0</span>
+			<div class="button">-</div>
+		</div>
+	</div>
+</template>
+
+<style>
+.home {
+	text-align: center;
+	padding: 20px;
+}
+
+.button, .counter {
+	font-size: 40px;
+	margin: 10px;
+}
+
+</style>
